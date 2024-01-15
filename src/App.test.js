c4124bee8d8db948aed9741bd387a484f9e2d906
Matt G <matt@mattrgordon.com>
2024-01-15 11:37:36 -0600

    attempting creating tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders weather stations header', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headerText = screen.getByText(/weather stations in minnesota/i);
+  expect(headerText).toBeInTheDocument();
 });
