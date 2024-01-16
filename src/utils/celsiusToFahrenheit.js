96ca30b6d67ef42ce923cdcfb9c37d475da956d1
Matt G <matt@mattrgordon.com>
2024-01-16 15:46:36 -0600

    not needed

--- src/utils/celsiusToFahrenheit.js
+++
@@ -1,7 +0,0 @@
-function celsiusToFahrenheit(celsius) {
-  // Formula: Fahrenheit = (Celsius * 9/5) + 32
-  let value = (celsius * 9) / 5 + 32;
-  return Math.round(value);
-}
-
-export default celsiusToFahrenheit;
