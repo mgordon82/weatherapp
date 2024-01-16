0657356b5f8e14a3148cd733a4d98f1b41dc9cc1
Matt G <matt@mattrgordon.com>
2024-01-16 15:46:03 -0600

    updated title

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -4,7 +4,7 @@ import LocalCard from '../localCard/localCard.component';
 const Header = ({ localForecast }) => {
   return (
     <header className='header'>
-      <h1>Weather Stations in Minnesota</h1>
+      <h1>Favorite Weather Locations</h1>
       <LocalCard localForecast={localForecast} />
     </header>
   );
